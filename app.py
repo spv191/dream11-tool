ae99918055562b611556bdec8d74062932b7ea4b
spv191 <spv191@gmail.com>
2025-02-26 18:21:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Dream11 AI Strategy Tool")  
+st.write("📊 Analyze player stats, past performances & optimize your team.")  
+
+st.text_input("Enter Player Name")  
+st.button("Analyze Player")
