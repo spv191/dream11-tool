09a0bc6400366491ee6ab8e9ca78b4aea48e2994
spv191 <spv191@gmail.com>
2025-02-27 11:34:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 import streamlit as st
+import traceback
 
+try
 st.title("Dream11 AI Strategy Tool")  
 st.write("📊 Analyze player stats, past performances & optimize your team.")  
 
 st.text_input("Enter Player Name")  
 st.button("Analyze Player")
+st.write(App is running...")
+
+except Exception as e:
+st error(f"Error:{str(e)}")#Display error in the app
+st.text((traceback.format_exc())show full error details
